0ec5ca357f3e6444e441df43e437443665223dcb
RMalinowski <r.malinowski3211@gmail.com>
2026-04-29 12:58:15 +0200

    test(endless): computing simmilarity

--- endless/features/scoreEngine/scoreEngine.test.cpp
+++ endless/features/scoreEngine/scoreEngine.test.cpp
@@ -2,9 +2,38 @@
 #include "scoreEngine.hpp"
 #include "../tokenizer/tokenizer.hpp"
 
-TEST(group, test1){
+TEST(scoreEngine, computesSimmilarity){
     Tokenizer tokenizer;
     ScoreEngine scoreEngine(tokenizer);
 
-    EXPECT_NEAR(0.333f, 0.333f, 0.001f);
+    Tokens first = {"hello", "driver", "one"};
+    Tokens second = {"hello", "driver", "two"};
+
+    float simmilarity = scoreEngine.computeSimmilarity(first, second);
+
+    EXPECT_NEAR(simmilarity, 0.5f, 0.001f);
+}
+
+TEST(scoreEngine, computesSimmilarityWhenEqual){
+    Tokenizer tokenizer;
+    ScoreEngine scoreEngine(tokenizer);
+
+    Tokens first = {"hello", "driver", "one"};
+    Tokens second = {"hello", "driver", "one"};
+
+    float simmilarity = scoreEngine.computeSimmilarity(first, second);
+
+    EXPECT_NEAR(simmilarity, 1.0f, 0.001f);
+}
+
+TEST(scoreEngine, handlesZeroSimmilarity){
+    Tokenizer tokenizer;
+    ScoreEngine scoreEngine(tokenizer);
+
+    Tokens first = {"hello", "driver", "one"};
+    Tokens second = {"hi", "passenger", "two"};
+
+    float simmilarity = scoreEngine.computeSimmilarity(first, second);
+
+    EXPECT_NEAR(simmilarity, 0.0f, 0.001f);
 }
